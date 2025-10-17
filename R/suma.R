6089085961d4ca1661317f65eda21b1d42c537fb
Kiara Manacasa <kmanacasa@gmail.com>
2025-10-17 11:21:42 -0300

    agrega paquetes

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,11 @@
-suma <- function (x,y) {
-  x+y
+suma <- function(x = 0, y = 0) {
+  if (!is.numeric(x) || !is.numeric(y)) {
+    stop("Ambos argumentos deben ser numéricos.")
+  }
+
+  if (x < 0 || y < 0) {
+    return("No puedo sumar negativos")
+  }
+
+  x + y
 }
---
+++ datarow/datos.R
@@ -0,0 +1,3 @@
+#leer, procesar o generar datos
+datos<- sample(1000)
+usethis::use_data(datos)
